74e1431de4de5e17726b9b7699e25f8fbbb50d39
antoniolg <antonio@devexperto.com>
2022-07-08 13:54:47 +0200

    Build MainFragment and make activity load it

--- app/src/main/java/com/devexperto/movietrailerstv/MainActivity.kt
+++ app/src/main/java/com/devexperto/movietrailerstv/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.devexperto.movietrailerstv
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : Activity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/devexperto/movietrailerstv/MainFragment.kt
@@ -0,0 +1,6 @@
+package com.devexperto.movietrailerstv
+
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+}

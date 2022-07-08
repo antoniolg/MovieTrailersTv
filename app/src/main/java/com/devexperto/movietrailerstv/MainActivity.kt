64993cedf608fa9056a45bf79753c023bbdfae7c
antoniolg <antonio@devexperto.com>
2022-07-08 13:51:47 +0200

    Make app run

--- app/src/main/java/com/devexperto/movietrailerstv/MainActivity.kt
+++ app/src/main/java/com/devexperto/movietrailerstv/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.devexperto.movietrailerstv
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

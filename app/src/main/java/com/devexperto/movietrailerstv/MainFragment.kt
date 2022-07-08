461e187ee7b232873ab74fde956d27cd7cbf328f
antoniolg <antonio@devexperto.com>
2022-07-08 23:38:27 +0200

    First implementation of BrowseSupportFragment

--- app/src/main/java/com/devexperto/movietrailerstv/MainFragment.kt
+++ app/src/main/java/com/devexperto/movietrailerstv/MainFragment.kt
@@ -2,7 +2,10 @@ package com.devexperto.movietrailerstv
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.leanback.app.BrowseSupportFragment
+import androidx.leanback.widget.*
 
 class MainFragment : BrowseSupportFragment() {
 
@@ -10,5 +13,49 @@ class MainFragment : BrowseSupportFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         title = getString(R.string.browse_title)
+
+        val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
+        val cardPresenter = CardPresenter()
+
+        (1..5).forEach { categoryId ->
+            val categoryTitle = "Category $categoryId"
+
+            val listRowAdapter = ArrayObjectAdapter(cardPresenter).apply {
+                addAll(0, (1..10).map { "Title $it" })
+            }
+
+            val header = HeaderItem(categoryId.toLong(), categoryTitle)
+            rowsAdapter.add(ListRow(header, listRowAdapter))
+        }
+
+        adapter = rowsAdapter
     }
+
+    override fun setHeaderPresenterSelector(headerPresenterSelector: PresenterSelector?) {
+        super.setHeaderPresenterSelector(headerPresenterSelector)
+    }
+}
+
+class CardPresenter : Presenter() {
+
+    override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
+
+        val cardView = ImageCardView(parent.context)
+        cardView.isFocusable = true
+        cardView.isFocusableInTouchMode = true
+
+        return ViewHolder(cardView)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
+        val title = item as String
+        val cardView = viewHolder.view as ImageCardView
+
+        cardView.titleText = title
+        cardView.setMainImageDimensions(176, 313)
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder) {
+    }
+
 }
